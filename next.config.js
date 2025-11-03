2e0ea0503ae2d13bd65352ed21786aefeec8ff94
josegomez-dev <josegomez.dev@gmail.com>
2025-11-03 13:41:44 -0600

    fix(csp): add missing Firebase API endpoints to CSP

--- next.config.js
+++ next.config.js
@@ -43,7 +43,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com data:",
               "img-src 'self' data: https: blob:",
-              "connect-src 'self' https://horizon-testnet.stellar.org https://horizon.stellar.org https://www.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://*.googleapis.com https://www.googletagmanager.com wss://horizon-testnet.stellar.org wss://horizon.stellar.org",
+              "connect-src 'self' https://horizon-testnet.stellar.org https://horizon.stellar.org https://www.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://firebaselogging.googleapis.com https://firebaseremoteconfig.googleapis.com https://www.googletagmanager.com wss://horizon-testnet.stellar.org wss://horizon.stellar.org",
               "frame-src 'self' https://*.youtube.com https://www.youtube.com",
               "base-uri 'self'",
               "form-action 'self'",
